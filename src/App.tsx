20f4deaaf4cfd21b3d455ec11cbea9e16c25bc3c
Anna Maria <aannamariabr@outlook.com>
2022-07-22 10:15:09 -0300

    Refatoração nos contextos para que eles funcionem em diferentes rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 // import { Context } from './Context'
 
 export function App() {
   return (
     // <Context />
-
     <>
       <ThemeProvider theme={defaultTheme}>
         <BrowserRouter>
-          <Router />
+          <CyclesContextProvider>
+            <Router />
+          </CyclesContextProvider>
           <GlobalStyle />
         </BrowserRouter>
       </ThemeProvider>
